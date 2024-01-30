6323be31ffa444f5568554a70b15ce0c79d63b7d
JordanGoasdoue <jordan.goasdoue@dailymotion.com>
2024-01-30 12:08:12 +0100

    feat: improve the display of repositories pr url at the end

--- repository/strategy_append.go
+++ repository/strategy_append.go
@@ -44,36 +44,36 @@ func (s *AppendStrategy) Run(ctx context.Context) (bool, *github.PullRequest, er
 		})
 	}
 	if err != nil {
-		return false, nil, fmt.Errorf("failed to switch to branch %s: %w", branchName, err)
+		return false, existingPR, fmt.Errorf("failed to switch to branch %s: %w", branchName, err)
 	}
 
 	repoUpdated, err := s.Repository.runUpdaters(ctx, s.Updaters, s.RepoPath)
 	if err != nil {
-		return false, nil, fmt.Errorf("failed to update repository %s: %w", s.Repository.FullName(), err)
+		return false, existingPR, fmt.Errorf("failed to update repository %s: %w", s.Repository.FullName(), err)
 	}
 	if !repoUpdated {
-		return false, nil, nil
+		return false, existingPR, nil
 	}
 
 	if err = s.Options.Git.setDefaultValues(s.Updaters, templateExecutorFor(s.Options, s.Repository, s.RepoPath)); err != nil {
-		return false, nil, fmt.Errorf("failed to set default git values: %w", err)
+		return false, existingPR, fmt.Errorf("failed to set default git values: %w", err)
 	}
 	if err = s.Options.GitHub.setDefaultValues(s.Options.Git, templateExecutorFor(s.Options, s.Repository, s.RepoPath)); err != nil {
-		return false, nil, fmt.Errorf("failed to set default github values: %w", err)
+		return false, existingPR, fmt.Errorf("failed to set default github values: %w", err)
 	}
 	s.Options.GitHub.setDefaultUpdateOperation(IgnoreUpdateOperation)
 
 	changesCommitted, err := commitChanges(ctx, gitRepo, s.Options)
 	if err != nil {
-		return false, nil, fmt.Errorf("failed to commit changes to git repository %s: %w", s.Repository.FullName(), err)
+		return false, existingPR, fmt.Errorf("failed to commit changes to git repository %s: %w", s.Repository.FullName(), err)
 	}
 	if !changesCommitted {
 		logrus.WithField("repository", s.Repository.FullName()).Debug("No changes recorded, nothing to push")
-		return false, nil, nil
+		return false, existingPR, nil
 	}
 	if s.Options.DryRun {
 		logrus.WithField("repository", s.Repository.FullName()).Warning("Running in dry-run mode, not pushing changes")
-		return false, nil, nil
+		return false, existingPR, nil
 	}
 
 	err = pushChanges(ctx, gitRepo, pushOptions{
@@ -81,7 +81,7 @@ func (s *AppendStrategy) Run(ctx context.Context) (bool, *github.PullRequest, er
 		BranchName: branchName,
 	})
 	if err != nil {
-		return false, nil, fmt.Errorf("failed to push changes to git repository %s: %w", s.Repository.FullName(), err)
+		return false, existingPR, fmt.Errorf("failed to push changes to git repository %s: %w", s.Repository.FullName(), err)
 	}
 
 	var pr *github.PullRequest
@@ -91,7 +91,7 @@ func (s *AppendStrategy) Run(ctx context.Context) (bool, *github.PullRequest, er
 		pr, err = s.Repository.createPullRequest(ctx, s.Options.GitHub, branchName)
 	}
 	if err != nil {
-		return false, nil, fmt.Errorf("failed to create or update Pull Request: %w", err)
+		return false, existingPR, fmt.Errorf("failed to create or update Pull Request: %w", err)
 	}
 
 	return true, pr, nil
